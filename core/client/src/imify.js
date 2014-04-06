c4bac57624f3335b6059dea085850eebd4a1b2b2
Richard Edwards <richard.edwards2@bbc.co.uk>
2014-04-06 23:05:18 +0100

    Moved app over to use requirejs via bower

--- core/client/src/imify.js
+++
@@ -1,1 +0,0 @@
-var imifyApp = angular.module('imifyApp', []);
--- core/client/src/shared/directive/ng-enter.js
+++ core/client/src/shared/directive/ng-enter.js
@@ -1,12 +1,18 @@
-imifyApp.directive('ngEnter', function () {
-  return function ($scope, element, attrs) {
-    element.bind('keydown keypress', function (event) {
-      if (event.which === 13) {
-        event.preventDefault();
-        $scope.$apply(function () {
-          $scope.$eval(attrs.ngEnter);
-        });
-      }
-    });
-  };
+'use strict';
+
+define(function (require) {
+  var app = require('app');
+
+  return app.directive('ngEnter', function () {
+    return function ($scope, element, attrs) {
+      element.bind('keydown keypress', function (event) {
+        if (event.which === 13) {
+          event.preventDefault();
+          $scope.$apply(function () {
+            $scope.$eval(attrs.ngEnter);
+          });
+        }
+      });
+    };
+  });
 });
