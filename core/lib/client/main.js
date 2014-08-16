d53d60a80607481e0788988354ebefcc3adbedb4
Richard Leap <richard@apergy.co.uk>
2014-08-16 21:44:04 +0100

    Made the application testable

--- core/lib/client/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var app = require('./app');
-
-app.start();
-
-module.exports = window.app = app;
--- grunt/jasmine.js
+++ grunt/jasmine.js
@@ -4,6 +4,7 @@ module.exports = function () {
   return {
     src: 'dist/app_bundle.js',
     options: {
+      keepRunner: process.env.GRUNT_ENV === 'development',
       specs: 'dist/test_bundle.js'
     }
   };
